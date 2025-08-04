3d521524d51610366bbbeec5b8c28263258f519c
Camila Sepulveda <camila.sepulveda.v@gmail.com>
2025-08-04 17:07:18 -0400

    Actualización carpeta JS y tarea pendiente CSS(ongoing)

---
+++ 02fundamentos/js/ejercicio4.js
@@ -0,0 +1,6 @@
+const alumnos = [
+  { nombre: "cami", apellido: "sepulveda", edad: 35, estadoCivil: "soltera" },
+  { nombre: "friza", apellido: "veliz", edad: 31, estadoCivil: "soltera" },
+  { nombre: "jose", apellido: "rodriguez", edad: 24, estadoCivil: "soltero" },
+  { nombre: "guille", apellido: "mezzano", edad: 36, estadoCivil: "soltero" },
+];
