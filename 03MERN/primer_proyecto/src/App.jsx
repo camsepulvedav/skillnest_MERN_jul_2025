a9ca8493316804256932a9a91c0b8d2f3355cf82
Camila Sepulveda <camila.sepulveda.v@gmail.com>
2025-08-27 22:54:26 -0400

    Tarea React con Vite

---
+++ 03MERN/primer_proyecto/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>¡Bienvenido a mi aplicación de React!</h1>
+      <h2>Lista de cosas por hacer:</h2>
+      <ul>
+          <li>Aprender React.</li>
+          <li>Practicar con Vite.</li>
+          <li>Construir proyectos increíbles.</li>
+      </ul>
+    </>
+  )
+}
+
+export default App
--- 03MERN/skillnest/src/App.jsx
+++ 03MERN/skillnest/src/App.jsx
@@ -11,19 +11,11 @@ const App = () =>{
   //HTML  
   return (
     <div className ="mainContainer">
-      {/* <h1>¡Bienvenido a mi aplicación de React!</h1>
-      <h2>Lista de cosas por hacer:</h2>
-      <ul>
-        <li>Aprender React</li>
-        <li>Practicar con Vite</li>
-        <li>Construir proyectos increíbles</li>
-      </ul> */}
       <NavBar/>
       <div className="subContainer">
         <Main/>
         <Side/>
       </div>
-        
     </div>
   )
 }
